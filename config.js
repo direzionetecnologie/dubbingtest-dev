2a408a443a812ba50bfd062e9ab9104d2c8d98d4
direzionetecnologie <valentinasag2@gmail.com>
2025-09-24 11:02:22 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+window.CONFIG = {
+  SURVEY_TITLE: 'Questionario sulla qualità del doppiaggio IA',
+  FORM_ENDPOINT: 'https://formsubmit.co/ajax/tech.transformation.mdst@gmail.com',
+  VIDEO_BASE_URL: 'https://aiworkshopmediaset.s3.eu-west-1.amazonaws.com/video/',
+  videos: [
+    'A1_SQUADRA_ANTIMAFIA.mp4',
+    'B2_LIQUID_UNIVERSE.mp4',
+    'C2_LA_CASA_FUERTE.mp4',
+    'B2_LIQUID_UNIVERSE.mp4',
+    'A3_CHICAGO_FIRE.mp4',
+    'A2_YARGI.mp4',
+    'B1_VIAGGIO_NELLA_GRANDE_BELLEZZA.mp4',
+    'A1_SQUADRA_ANTIMAFIA.mp4',
+    'A1_SQUADRA_ANTIMAFIA.mp4',
+    'A1_SQUADRA_ANTIMAFIA.mp4'
+  ]
+};
